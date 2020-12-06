749d72a437496619d8140340c70023019fe418c4
Jon McManus <due@wxwhatever.com>
2020-12-06 21:45:49 +1000

    Rocky Peaks.

--- src/main/java/lykrast/voyage/biomes/RockyPeaksBiome.java
+++ src/main/java/lykrast/voyage/biomes/RockyPeaksBiome.java
@@ -1,16 +1,22 @@
 package lykrast.voyage.biomes;
 
+import lykrast.voyage.ColorConstants;
 import lykrast.voyage.VoyageBiomes;
 import lykrast.voyage.biomebuilder.DefaultFeature;
 import lykrast.voyage.init.ConfiguredSurfaceBuilders;
 import lykrast.voyage.init.ModFeatures;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.GenerationStage;
 
 public class RockyPeaksBiome {
   public static Biome ROCKY_PEAKS = VoyageBiomes.BIOME_TEMPLATE.builder()
+       .effects(VoyageBiomes.createDefaultBiomeAmbience()
+          .setWaterColor(ColorConstants.STANDARD_WATER)
+          .setWaterFogColor(ColorConstants.STANDARD_WATERFOG))
       .surfaceBuilder(ConfiguredSurfaceBuilders.CONFIGURED_PEAKS)
       .precipitation(Biome.RainType.RAIN)
       .category(Biome.Category.EXTREME_HILLS)
@@ -18,16 +24,24 @@ public class RockyPeaksBiome {
       .scale(1.1F)
       .temperature(0.7F)
       .downfall(0.8F)
-      .addDefaultFeatures(DefaultFeature.DEFAULT_UNDERGROUND_STRUCTURES, DefaultFeature.LAND_CARVERS, DefaultFeature.LAKES, DefaultFeature.DUNGEONS, DefaultFeature.DISKS, DefaultFeature.SPRINGS, DefaultFeature.DEFAULT_MUSHROOMS, DefaultFeature.EMERALD_ORE, DefaultFeature.INFECTED_STONE, DefaultFeature.FROZEN_TOP_LAYER)
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.BAT, 10, 8, 8))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SPIDER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE, 95, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE_VILLAGER, 5, 1, 1))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SKELETON, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.CREEPER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SLIME, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ENDERMAN, 10, 1, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.WITCH, 5, 1, 1))
-      .addFeature(GenerationStage.Decoration.LOCAL_MODIFICATIONS, ModFeatures.ROCK_BOULDERS)
+      .addDefaultFeatureFunctions(
+          DefaultBiomeFeatures::withStrongholdAndMineshaft,
+          DefaultBiomeFeatures::withCavesAndCanyons,
+          DefaultBiomeFeatures::withLavaAndWaterLakes,
+          DefaultBiomeFeatures::withMonsterRoom,
+          DefaultBiomeFeatures::withCommonOverworldBlocks,
+          DefaultBiomeFeatures::withOverworldOres,
+          DefaultBiomeFeatures::withDisks,
+          DefaultBiomeFeatures::withLavaAndWaterSprings,
+          DefaultBiomeFeatures::withMushroomBiomeVegetation,
+          DefaultBiomeFeatures::withEmeraldOre,
+          DefaultBiomeFeatures::withInfestedStone,
+          DefaultBiomeFeatures::withFrozenTopLayer
+      )
+      .addSpawnFunctions(
+          DefaultBiomeFeatures::withBatsAndHostiles,
+          (o) -> o.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.LLAMA, 5, 4, 6))
+      )
+      .addFeature(GenerationStage.Decoration.LOCAL_MODIFICATIONS, ModFeatures.ROCK_BOULDERS) // TODO: IMPROVE
       .build();
 }
