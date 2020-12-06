4d655c57c222875a629f05bab35775ebc07db347
Jon McManus <due@wxwhatever.com>
2020-12-06 21:35:09 +1000

    Lagoons.

--- src/main/java/lykrast/voyage/biomes/LagoonBiome.java
+++ src/main/java/lykrast/voyage/biomes/LagoonBiome.java
@@ -3,40 +3,44 @@ package lykrast.voyage.biomes;
 import lykrast.voyage.ColorConstants;
 import lykrast.voyage.VoyageBiomes;
 import lykrast.voyage.biomebuilder.BiomeTemplate;
-import lykrast.voyage.biomebuilder.DefaultFeature;
 import lykrast.voyage.init.ConfiguredSurfaceBuilders;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.structure.StructureFeatures;
 
 public class LagoonBiome {
   private static final BiomeTemplate LAGOON_TEMPLATE = new BiomeTemplate(VoyageBiomes.BIOME_TEMPLATE.builder()
       .surfaceBuilder(ConfiguredSurfaceBuilders.CONFIGURED_DEFAULT_GRASS)
       .precipitation(Biome.RainType.RAIN)
       .category(Biome.Category.BEACH)
-      .addDefaultFeatures(DefaultFeature.AQUATIC_STRUCTURES, DefaultFeature.OCEAN_CARVERS, DefaultFeature.DEFAULT_UNDERGROUND_STRUCTURES, DefaultFeature.LAKES, DefaultFeature.DUNGEONS, DefaultFeature.DISKS, DefaultFeature.DEFAULT_FLOWERS, DefaultFeature.DEFAULT_MUSHROOMS, DefaultFeature.SPRINGS, DefaultFeature.FROZEN_TOP_LAYER, DefaultFeature.DEFAULT_VEGETATION, DefaultFeature.KELP, DefaultFeature.BADLANDS_GRASS, DefaultFeature.DEFAULT_GRASS)
-      .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.TREES_WATER)
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SQUID, 3, 1, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.COD, 15, 3, 6))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SALMON, 15, 1, 5))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.BAT, 10, 8, 8))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SPIDER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE, 95, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.DROWNED, 5, 1, 1))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE_VILLAGER, 5, 1, 1))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SKELETON, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.CREEPER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SLIME, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ENDERMAN, 10, 1, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.WITCH, 5, 1, 1)));
+      .addDefaultFeatureFunctions(
+          DefaultBiomeFeatures::withOceanCavesAndCanyons,
+          DefaultBiomeFeatures::withOceanStructures,
+          DefaultBiomeFeatures::withLavaAndWaterLakes,
+          DefaultBiomeFeatures::withMonsterRoom,
+          DefaultBiomeFeatures::withCommonOverworldBlocks,
+          DefaultBiomeFeatures::withOverworldOres,
+          DefaultBiomeFeatures::withDisks,
+          DefaultBiomeFeatures::withTreesInWater, // addScatteredOakTrees
+          DefaultBiomeFeatures::withDefaultFlowers,
+          DefaultBiomeFeatures::withNormalMushroomGeneration,
+          DefaultBiomeFeatures::withSugarCaneAndPumpkins,
+          DefaultBiomeFeatures::withLavaAndWaterSprings,
+          DefaultBiomeFeatures::withFrozenTopLayer
+      )
+      .addStructureFeature(StructureFeatures.BURIED_TREASURE));
 
   public static Biome LAGOON_COLD = LAGOON_TEMPLATE.builder()
       .effects(VoyageBiomes.createDefaultBiomeAmbience()
           .setWaterColor(ColorConstants.OCEAN_COLD_WATER)
           .setWaterFogColor(ColorConstants.STANDARD_WATERFOG))
       .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SEAGRASS_COLD)
+      .addSpawnFunction((o) -> DefaultBiomeFeatures.withOceanMobs(o, 3, 4, 15))
+      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SALMON, 15, 1, 5))
       .depth(-0.3F)
       .scale(0.0125F)
       .temperature(0.5F)
@@ -47,8 +51,15 @@ public class LagoonBiome {
       .effects(VoyageBiomes.createDefaultBiomeAmbience()
           .setWaterColor(ColorConstants.OCEAN_LUKEWARM_WATER)
           .setWaterFogColor(ColorConstants.OCEAN_LUKEWARM_WATERFOG))
+      .addDefaultFeatureFunctions(
+          DefaultBiomeFeatures::withWarmKelp,
+          DefaultBiomeFeatures::withNormalGrassPatch
+      )
+      .addSpawnFunction((o) -> DefaultBiomeFeatures.withOceanMobs(o, 3, 4, 15))
+      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.PUFFERFISH, 5, 1, 3))
+      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.TROPICAL_FISH, 25, 8, 8))
+      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.DOLPHIN, 2, 1, 2))
       .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SEAGRASS_WARM)
-      .addDefaultFeatures(DefaultFeature.SEAGRASS_ON_STONE)
       .depth(-0.3F)
       .scale(0.0125F)
       .temperature(0.5F)
@@ -59,14 +70,20 @@ public class LagoonBiome {
       .effects(VoyageBiomes.createDefaultBiomeAmbience()
           .setWaterColor(ColorConstants.OCEAN_LUKEWARM_WATER)
           .setWaterFogColor(ColorConstants.OCEAN_LUKEWARM_WATERFOG))
+      .addDefaultFeatureFunctions(
+          DefaultBiomeFeatures::withBadlandsGrass
+      )
+      .addSpawnFunctions(
+          (o) -> DefaultBiomeFeatures.withOceanMobs(o, 1, 4, 10),
+          (o) -> DefaultBiomeFeatures.withWarmOceanMobs(o, 10, 4))
+      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.DOLPHIN, 1, 1, 2))
       .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.WARM_OCEAN_VEGETATION)
+      .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SEAGRASS_WARM)
       .addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Features.SEA_PICKLE)
       .depth(-0.3F)
       .scale(0.0125F)
       .temperature(0.5F)
       .downfall(0.5F)
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.PUFFERFISH, 15, 1, 3))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.TROPICAL_FISH, 25, 8, 8))
       .build();
 
 }
--- src/main/java/lykrast/voyage/init/ModFeatures.java
+++ src/main/java/lykrast/voyage/init/ModFeatures.java
@@ -1,29 +1,25 @@
 package lykrast.voyage.init;
 
 import com.google.common.collect.ImmutableList;
-import com.tterrag.registrate.util.entry.RegistryEntry;
 import lykrast.voyage.Voyage;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
-import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
-import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.placement.Placement;
 
 @SuppressWarnings("unchecked")
 public class ModFeatures {
-  public static ConfiguredFeature<?, ?> LUSH_TREES = register("lush_trees",
   public static ConfiguredFeature<?, ?> ROCK_BOULDERS = register("rock_boulders", Feature.RANDOM_SELECTOR.withConfiguration(
-        new MultipleRandomFeatureConfig(
-      ImmutableList.of(
-                Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.COBBLESTONE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f),
-                Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.ANDESITE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f),
-                Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.GRANITE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f))
-            ,
-            Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.COBBLESTONE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).func_242732_c(2))
-        ).withPlacement(Placement.COUNT.configure(new FeatureSpreadConfig(6))));
+      new MultipleRandomFeatureConfig(
+          ImmutableList.of(
+              Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.COBBLESTONE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f),
+              Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.ANDESITE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f),
+              Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.GRANITE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).withChance(0.1f))
+          ,
+          Feature.FOREST_ROCK.withConfiguration(new BlockStateFeatureConfig(Blocks.COBBLESTONE.getDefaultState())).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).func_242732_c(2))
+  ).withPlacement(Placement.COUNT.configure(new FeatureSpreadConfig(6))));
 
   private static ConfiguredFeature<?, ?> register(String id, ConfiguredFeature<?, ?> feature) {
     return Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(Voyage.MODID, id), feature);
