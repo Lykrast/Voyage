dcc902755e3a68c7c6dff85849bb16edbf07c16e
Jon McManus <due@wxwhatever.com>
2020-12-06 21:43:08 +1000

    Rock field (still need to improve rocks)

--- src/main/java/lykrast/voyage/biomes/RockFieldBiome.java
+++ src/main/java/lykrast/voyage/biomes/RockFieldBiome.java
@@ -5,13 +5,17 @@ import lykrast.voyage.VoyageBiomes;
 import lykrast.voyage.biomebuilder.DefaultFeature;
 import lykrast.voyage.init.ConfiguredSurfaceBuilders;
 import lykrast.voyage.init.ModFeatures;
-import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.gen.GenerationStage;
 
 public class RockFieldBiome {
   public static Biome ROCK_FIELD = VoyageBiomes.BIOME_TEMPLATE.builder()
+      .effects(VoyageBiomes.createDefaultBiomeAmbience()
+          .withGrassColor(0xFFEE96)
+          .withFoliageColor(0xFFEE96)
+          .setWaterColor(ColorConstants.STANDARD_WATER)
+          .setWaterFogColor(ColorConstants.STANDARD_WATERFOG))
       .surfaceBuilder(ConfiguredSurfaceBuilders.CONFIGURED_ROCKY)
       .precipitation(Biome.RainType.RAIN)
       .category(Biome.Category.SAVANNA)
@@ -19,21 +23,20 @@ public class RockFieldBiome {
       .scale(0.2F)
       .temperature(2.0F)
       .downfall(0.0F)
-      .effects(VoyageBiomes.createDefaultBiomeAmbience()
-          .withGrassColor(0xFFEE96)
-          .withFoliageColor(0xFFEE96)
-          .setWaterColor(ColorConstants.STANDARD_WATER)
-          .setWaterFogColor(ColorConstants.STANDARD_WATERFOG))
-      .addDefaultFeatures(DefaultFeature.BADLANDS_UNDERGROUND_STRUCTURES, DefaultFeature.LAND_CARVERS, DefaultFeature.LAKES, DefaultFeature.DUNGEONS, DefaultFeature.DISKS, DefaultFeature.DEFAULT_MUSHROOMS, DefaultFeature.SPRINGS, DefaultFeature.DEFAULT_VEGETATION, DefaultFeature.FROZEN_TOP_LAYER, DefaultFeature.BADLANDS_GRASS)
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.BAT, 10, 8, 8))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SPIDER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE, 95, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ZOMBIE_VILLAGER, 5, 1, 1))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SKELETON, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.CREEPER, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.SLIME, 100, 4, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.ENDERMAN, 10, 1, 4))
-      .addSpawnEntry(new MobSpawnInfo.Spawners(EntityType.WITCH, 5, 1, 1))
-      .addFeature(GenerationStage.Decoration.LOCAL_MODIFICATIONS, ModFeatures.ROCK_BOULDERS)
+      .addDefaultFeatureFunctions(
+          DefaultBiomeFeatures::withBadlandsStructures,
+          DefaultBiomeFeatures::withCavesAndCanyons,
+          DefaultBiomeFeatures::withLavaAndWaterLakes,
+          DefaultBiomeFeatures::withMonsterRoom,
+          DefaultBiomeFeatures::withCommonOverworldBlocks,
+          DefaultBiomeFeatures::withOverworldOres,
+          DefaultBiomeFeatures::withDisks,
+          DefaultBiomeFeatures::withBadlandsGrassAndBush,
+          DefaultBiomeFeatures::withMushroomBiomeVegetation,
+          DefaultBiomeFeatures::withLavaAndWaterSprings,
+          DefaultBiomeFeatures::withFrozenTopLayer
+      )
+      .addSpawnFunction(DefaultBiomeFeatures::withBatsAndHostiles)
+      .addFeature(GenerationStage.Decoration.LOCAL_MODIFICATIONS, ModFeatures.ROCK_BOULDERS) // TODO: Improve this
       .build();
 }
